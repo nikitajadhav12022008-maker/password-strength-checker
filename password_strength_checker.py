9361bf099312893b67a31ba473ac345103e95d8c
Nikita Jadhav <nikita.jadhav12022008@gmail.com>
2026-02-20 06:36:13 -0800

    Updated password_strength_checker.py

--- password_strength_checker.py
+++ password_strength_checker.py
@@ -1,19 +1,19 @@
 #python based password strength checker:
 def check_password_strength(password):
     score = 0
-
+#length check
     if len(password) >= 8:
         score += 1
-
+#uppercase check
     if any(char.isupper() for char in password):
         score += 1
-
+#lowercase check
     if any(char.islower() for char in password):
         score += 1
-
+digit check
     if any(char.isdigit() for char in password):
         score += 1
-
+#special character check
     special_chars = "!@#$%^&*()-_+=<>?/|"
     if any(char in special_chars for char in password):
         score += 1
@@ -26,7 +26,7 @@ def show_result(score):
         return "medium"
     else:
         return "strong"
-    
+ #main program   
 print("=========password strength checker==============")
 password = input("enter your password:")
 
@@ -34,4 +34,5 @@ score = check_password_strength(password)
 result = show_result(score)
 
 print("\npassword strength:",result)
+
 
